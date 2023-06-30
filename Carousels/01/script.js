fdb42592483334ac165e81418d6ed10c91a0868b
parthwebdev <parth.webdev@gmail.com>
2023-06-30 16:18:23 +0530

    Added Carousels Part 01

---
+++ Carousels/01/script.js
@@ -0,0 +1,27 @@
+const slides = document.querySelectorAll("[data-slide]");
+const buttons = document.querySelectorAll("[data-button]");
+
+let currSlide = 0;
+let maxSlide = slides.length - 1;
+
+const updateCarousel = (number = 0) => {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${(index - number) * 100}%)`;
+  });
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.dataset.button == "next" ? ++currSlide : --currSlide;
+
+    if (currSlide > maxSlide) {
+      currSlide = 0;
+    } else if (currSlide < 0) {
+      currSlide = maxSlide;
+    }
+
+    updateCarousel(currSlide);
+  });
+});
+
+updateCarousel();
