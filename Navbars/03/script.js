8e368f20840366f7a0374624d3aa86b7ce666eae
parthwebdev <parth.webdev@gmail.com>
2023-09-14 13:39:02 +0530

    Added NavBars Part 03

---
+++ Navbars/03/script.js
@@ -0,0 +1,14 @@
+const navBar = document.querySelector(".navbar");
+let prevScrollPos = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  let currScrollPos = window.scrollY;
+
+  if (currScrollPos > prevScrollPos) {
+    navBar.style.transform = `translateY(-105%)`;
+  } else {
+    navBar.style.transform = `translateY(0%)`;
+  }
+
+  prevScrollPos = currScrollPos;
+});
