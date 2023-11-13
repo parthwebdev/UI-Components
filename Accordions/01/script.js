d23e962be3125eefbe5aa03f2dfb4f8ffb46b22a
parthwebdev <parth.webdev@gmail.com>
2023-11-13 13:50:11 +0530

    Added Accordions Part 01

---
+++ Accordions/01/script.js
@@ -0,0 +1,13 @@
+const cardsContainer = document.querySelector(".container");
+
+cardsContainer.addEventListener("click", (e) => {
+  const target = e.target.closest(".card");
+
+  if (!target) return;
+
+  cardsContainer.querySelectorAll(".card").forEach((card) => {
+    card.classList.remove("active");
+  });
+
+  target.classList.add("active");
+});
